5d5ddf86095df74753661eb07ec74d09516d0d84
Kevin Farst <farst.6@osu.edu>
2013-09-09 03:42:37 -0700

    fix specs for delayed job

--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -3,19 +3,27 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe SitesController do
   before(:each) { login_as :jordan }
 
+  before do
+    Delayed::Worker.delay_jobs = false
+  end
+
+  after do
+    Delayed::Worker.delay_jobs = true
+  end
+
   it 'handles / with GET' do
     get :index
     response.should be_success
   end
-  
+
   it 'handles /sites with valid parameters and POST' do
-    running { 
+    running {
       post :create, site: {url: 'http://google.com'}
       assigns(:site).user.should == @user
       response.should redirect_to(sites_path)
     }.should change(Site, :count).by(1)
   end
-  
+
   it 'handles /sites with invalid url and POST' do
     running {
       post :create, site: {url: 'invalid'}
@@ -23,16 +31,16 @@ describe SitesController do
       response.should render_template(:index)
     }.should_not change(Site, :count)
   end
-  
+
   it 'handles / with valid parameters and POST' do
-    running { 
+    running {
       post :api, url: 'http://google.com'
       assigns(:site).user.should == @user
       response.should be_success
       response.body.should ==  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<site>\n  <state>success</state>\n  <image-url>/sites/2/original/2-full.png</image-url>\n</site>\n"
     }.should change(Site, :count).by(1)
   end
-  
+
   it 'handles / with empty url and POST' do
     running {
       post :api
@@ -40,7 +48,7 @@ describe SitesController do
       response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Url is invalid</error>\n</errors>\n"
     }.should_not change(Site, :count)
   end
-  
+
   it 'handles /sites with invalid url and POST' do
     running {
       post :api, :url=>'invalid'
